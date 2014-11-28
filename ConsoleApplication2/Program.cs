3d91edf5632f1a63228d9712e60832ef160b29fa
unknown <neeraja.vedulla@invilogic011.invilogic.local>
2014-11-28 14:57:54 +0530

    first commit

---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+ public class mathhelper
+    {
+        public int add(int a, int b)
+        {
+            int x = a + b;
+            return x;
+        }
+        public int sub(int a, int b)
+        {
+            int x = a - b;
+            return x;
+        }
+    }
+}
---
+++ mathtest/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using ConsoleApplication2;
+namespace mathtest
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void addtest()
+        {
+            mathhelper obj = new mathhelper();
+            int result = obj.add(20, 10);
+            int expert = 30;
+            Assert.AreEqual(expert, result);
+        }
+
+    }
+}
